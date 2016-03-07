4ae7ce56cd580ca51b9c4257df38b9c031b93009
jonasto <jonasto@student.chalmers.se>
2016-03-07 14:22:16 +0100

    simple syntax fix

--- scripts/haar.py
+++ scripts/haar.py
@@ -5,7 +5,7 @@ from PIL import Image as edit
 def get_crops(xml, image_paths, crop_path):
     cascade=cv2.CascadeClassifier(xml)
     for image_path in image_paths:
-        int i = 0
+        i = 0
         image = edit.open(image_path)
         gray  = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         signs = cascade.detectMultiscale(gray,1.1,5,0,(10,10),(200,200))
