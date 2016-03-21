38d3edc997828df9612fbf8b8c7704b3badbe38f
Gunnar Snorri Ragnarsson <Gunnar.Snorri-Ragnarsson@qrtech.se>
2016-03-21 14:55:51 +0100

    haar: fixed syntax errors

--- scripts/haar.py
+++ scripts/haar.py
@@ -1,15 +1,24 @@
-import numpy as np
+import os
 import cv2
 
 
 def get_crops(xml, image_paths, crop_path):
-    cascade=cv2.CascadeClassifier(xml)
+    cascade = cv2.CascadeClassifier(xml)
     for image_path in image_paths:
         i = 0
         image = cv2.imread(image_path)
-        signs = cascade.detectMultiscale(image,scaleFactor=1.1,minNeighbors =5 ,minSize=(10,10),flags(cv2.cv.CV_HAAR_SCALE_IMAGE)
-        for (x,y,w,h) in signs:
-            crop_image_path=('%s/%s_%s')%(crop_path,i,image_path)
-            cropped_image=image[x:x+w,y:y+h]
-            cv2.imwrite(crop_img_path, cropped_image)
-            i+=1
+        # image,scaleFactor=1.1,minNeighbors =5
+        # ,minSize=(10,10),flags(cv2.cv.CV_HAAR_SCALE_IMAGE
+        signs = cascade.detectMultiScale(
+            image,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(100, 100),
+            maxSize=(200, 200),
+            )
+        for (x, y, w, h) in signs:
+            image_name = image_path.split("/")[-1]
+            crop_image_path = os.path.join(crop_path, "%s-%s" % (image_name, str(i)))
+            cropped_image = image[y:y+h, x:x+w]
+            cv2.imwrite(crop_image_path, cropped_image)
+            i += 1
