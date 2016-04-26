b3640d77fd2270f812a88b26105a364bcc755370
dnn user <you@example.com>
2016-04-26 11:57:44 +0200

    added evaluation functions

--- misc/traffic.py
+++ misc/traffic.py
@@ -8,25 +8,27 @@
 
 import os
 from datasets.imdb import imdb
-import datasets.ds_utils as ds_utils
+# import datasets.ds_utils as ds_utils
 import xml.etree.ElementTree as ET
 import numpy as np
 import h5py
 import scipy.sparse
-import utils.cython_bbox
+# import utils.cython_bbox
 import cPickle
-import subprocess
+# import subprocess
 import uuid
-# from voc_eval import voc_eval
-from fast_rcnn.config import cfg
+from voc_eval import voc_eval
+# from fast_rcnn.config import cfg
+
 
 class Traffic(imdb):
+
     def __init__(self, image_set, devkit_path=None):
         super(Traffic, self).__init__(image_set)
         self._image_set = image_set
         self._devkit_path = devkit_path
         self._data_path = os.path.join(self._devkit_path, 'data')
-        self._classes = ('__background__', # always index 0
+        self._classes = ('__background__',  # always index 0
                          'sign')
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_exts = ['.jpg', '.png', '.bmp']
@@ -37,17 +39,17 @@ class Traffic(imdb):
         self._comp_id = 'comp4'
 
         # PASCAL specific config options
-        self.config = {'cleanup'     : True,
-                       'use_salt'    : True,
-                       'use_diff'    : False,
-                       'matlab_eval' : False,
-                       'rpn_file'    : None,
-                       'min_size'    : 2}
+        self.config = {'cleanup': True,
+                       'use_salt': True,
+                       'use_diff': False,
+                       'matlab_eval': False,
+                       'rpn_file': None,
+                       'min_size': 2}
 
         assert os.path.exists(self._devkit_path), \
-                'VOCdevkit path does not exist: {}'.format(self._devkit_path)
+            'VOCdevkit path does not exist: {}'.format(self._devkit_path)
         assert os.path.exists(self._data_path), \
-                'Path does not exist: {}'.format(self._data_path)
+            'Path does not exist: {}'.format(self._data_path)
 
     def image_path_at(self, i):
         """
@@ -71,7 +73,7 @@ class Traffic(imdb):
         image_set_file = os.path.join(self._data_path, 'ImageSets',
                                       self._image_set + '.txt')
         assert os.path.exists(image_set_file), \
-                'Path does not exist: {}'.format(image_set_file)
+            'Path does not exist: {}'.format(image_set_file)
         with open(image_set_file) as f:
             image_index = [x.strip() for x in f.readlines()]
         return image_index
@@ -144,7 +146,7 @@ class Traffic(imdb):
         filename = self.config['rpn_file']
         print 'loading {}'.format(filename)
         assert os.path.exists(filename), \
-               'rpn data not found at: {}'.format(filename)
+            'rpn data not found at: {}'.format(filename)
         with open(filename, 'rb') as f:
             box_list = cPickle.load(f)
         return self.create_roidb_from_box_list(box_list, gt_roidb)
@@ -153,16 +155,16 @@ class Traffic(imdb):
         filename = os.path.abspath(os.path.join(self._data_path,
                                                 self.name + '.h5'))
         assert os.path.exists(filename), \
-               'Selective search data not found at: {}'.format(filename)
+            'Selective search data not found at: {}'.format(filename)
         box_file = h5py.File(filename)
         raw_box_data = box_file["boxes"]
 
         box_list = []
         for image in raw_box_data:
-            boxes = raw_box_data[image].value[:, (1, 0, 3, 2)]  # XXX: Check if correct
+            boxes = raw_box_data[image].value[
+                :, (1, 0, 3, 2)]  # XXX: Check if correct
             # filter?
             box_list.append(boxes)
-
 
         # for i in xrange(raw_data.shape[0]):
         #     boxes = raw_data[i][:, (1, 0, 3, 2)] - 1
@@ -210,82 +212,83 @@ class Traffic(imdb):
 
     def _get_comp_id(self):
         comp_id = (self._comp_id + '_' + self._salt if self.config['use_salt']
-            else self._comp_id)
+                   else self._comp_id)
         return comp_id
 
-    # def _get_voc_results_file_template(self):
-    #     # VOCdevkit/results/VOC2007/Main/<comp_id>_det_test_aeroplane.txt
-    #     filename = self._get_comp_id() + '_det_' + self._image_set + '_{:s}.txt'
-    #     path = os.path.join(
-    #         self._devkit_path,
-    #         'results',
-    #         'VOC' + self._year,
-    #         'Main',
-    #         filename)
-    #     return path
+    def _get_results_file_template(self):
+        filename = self._get_comp_id() + '_det_' + self._image_set + '_{:s}.txt'
+        path = os.path.join(
+            self._data_path,
+            'results',
+            filename)
+        return path
 
-    # def _write_voc_results_file(self, all_boxes):
-    #     for cls_ind, cls in enumerate(self.classes):
-    #         if cls == '__background__':
-    #             continue
-    #         print 'Writing {} VOC results file'.format(cls)
-    #         filename = self._get_voc_results_file_template().format(cls)
-    #         with open(filename, 'wt') as f:
-    #             for im_ind, index in enumerate(self.image_index):
-    #                 dets = all_boxes[cls_ind][im_ind]
-    #                 if dets == []:
-    #                     continue
-    #                 # the VOCdevkit expects 1-based indices
-    #                 for k in xrange(dets.shape[0]):
-    #                     f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
-    #                             format(index, dets[k, -1],
-    #                                    dets[k, 0] + 1, dets[k, 1] + 1,
-    #                                    dets[k, 2] + 1, dets[k, 3] + 1))
+    def _write_dataset_results_file(self, all_boxes):
+        use_salt = self.config['use_salt']
+        # Have to track this, i have no idea why this is set in stone.
+        comp_id = 'comp4'
+        if use_salt:
+            comp_id += '_{}'.format(os.getpid())  # diff between
+            # - and _ in inria and original?
+        path = os.path.join(
+            self._devkit_path,
+            'results',
+            self.name,
+            comp_id + '_')
 
-    # def _do_python_eval(self, output_dir = 'output'):
-    #     annopath = os.path.join(
-    #         self._devkit_path,
-    #         'VOC' + self._year,
-    #         'Annotations',
-    #         '{:s}.xml')
-    #     imagesetfile = os.path.join(
-    #         self._devkit_path,
-    #         'VOC' + self._year,
-    #         'ImageSets',
-    #         'Main',
-    #         self._image_set + '.txt')
-    #     cachedir = os.path.join(self._devkit_path, 'annotations_cache')
-    #     aps = []
-    #     # The PASCAL VOC metric changed in 2010
-    #     use_07_metric = True if int(self._year) < 2010 else False
-    #     print 'VOC07 metric? ' + ('Yes' if use_07_metric else 'No')
-    #     if not os.path.isdir(output_dir):
-    #         os.mkdir(output_dir)
-    #     for i, cls in enumerate(self._classes):
-    #         if cls == '__background__':
-    #             continue
-    #         filename = self._get_voc_results_file_template().format(cls)
-    #         rec, prec, ap = voc_eval(
-    #             filename, annopath, imagesetfile, cls, cachedir, ovthresh=0.5,
-    #             use_07_metric=use_07_metric)
-    #         aps += [ap]
-    #         print('AP for {} = {:.4f}'.format(cls, ap))
-    #         with open(os.path.join(output_dir, cls + '_pr.pkl'), 'w') as f:
-    #             cPickle.dump({'rec': rec, 'prec': prec, 'ap': ap}, f)
-    #     print('Mean AP = {:.4f}'.format(np.mean(aps)))
-    #     print('~~~~~~~~')
-    #     print('Results:')
-    #     for ap in aps:
-    #         print('{:.3f}'.format(ap))
-    #     print('{:.3f}'.format(np.mean(aps)))
-    #     print('~~~~~~~~')
-    #     print('')
-    #     print('--------------------------------------------------------------')
-    #     print('Results computed with the **unofficial** Python eval code.')
-    #     print('Results should be very close to the official MATLAB eval code.')
-    #     print('Recompute with `./tools/reval.py --matlab ...` for your paper.')
-    #     print('-- Thanks, The Management')
-    #     print('--------------------------------------------------------------')
+        for cls_ind, cls in enumerate(self.classes):
+            if cls == '__background__':
+                continue  # assumes continue skips this iteration of the loop.
+            print 'Writing {} results file'.format(cls)
+            filename = path + 'det_' + self._image_set + '_' + cls + '.txt'
+            with open(filename, 'wt') as f:
+                for im_ind, index in enumerate(self.image_index):
+                    dets = all_boxes[cls_ind][im_ind]
+                    if dets == []:
+                        continue
+                    for k in xrange(dets.shape[0]):
+                        f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
+                                format(index, dets[k, -1],
+                                       dets[k, 0] + 1, dets[k, 1] + 1,
+                                       dets[k, 2] + 1, dets[k, 3] + 1))
+        return comp_id
+
+    def _do_python_eval(self, output_dir='output'):
+        annopath = os.path.join(
+            self._data_path, 'Annotations', '{:s}.xml')
+        imagesetfile = os.path.join(
+            self._data_path, 'ImageSets', 'train.txt')
+        cachedir = os.path.join(self._data_path, 'annotations_cache')
+        aps = []
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir)
+        for i, cls in enumerate(self._classes):
+            if cls == '__background__':
+                continue
+            filename = self._get_results_file_template().format(cls)
+            try:
+                rec, prec, ap = voc_eval(
+                    filename,
+                    annopath,
+                    imagesetfile,
+                    cls,
+                    cachedir,
+                    ovthresh=0.5,
+                    use_07_metric=False)
+            except IOError:
+                continue
+            aps += [ap]
+
+            print('{:.3f}'.format(ap))
+        print('{:.3f}'.format(np.mean(aps)))
+        print('~~~~~~~~')
+        print('')
+        print('--------------------------------------------------------------')
+        print('Results computed with the **unofficial** Python eval code.')
+        print('Results should be very close to the official MATLAB eval code.')
+        print('Recompute with `./tools/reval.py --matlab ...` for your paper.')
+        print('-- Thanks, The Management')
+        print('--------------------------------------------------------------')
 
     # def _do_matlab_eval(self, output_dir='output'):
     #     print '-----------------------------------------------------'
@@ -302,17 +305,15 @@ class Traffic(imdb):
     #     print('Running:\n{}'.format(cmd))
     #     status = subprocess.call(cmd, shell=True)
 
-    # def evaluate_detections(self, all_boxes, output_dir):
-    #     self._write_voc_results_file(all_boxes)
-    #     self._do_python_eval(output_dir)
-    #     if self.config['matlab_eval']:
-    #         self._do_matlab_eval(output_dir)
-    #     if self.config['cleanup']:
-    #         for cls in self._classes:
-    #             if cls == '__background__':
-    #                 continue
-    #             filename = self._get_voc_results_file_template().format(cls)
-    #             os.remove(filename)
+    def evaluate_detections(self, all_boxes, output_dir):
+        self._write_voc_results_file(all_boxes)
+        self._do_python_eval(output_dir)
+        if self.config['cleanup']:
+            for cls in self._classes:
+                if cls == '__background__':
+                    continue
+                filename = self._get_results_file_template().format(cls)
+                os.remove(filename)
 
     def competition_mode(self, on):
         if on:
